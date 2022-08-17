e5b111ee08bc476490aa595117ae5621b6de3056
paoluoyue <13121133933@163.com>
2022-08-17 17:49:53 +0800

    /*  * @Author: paoluo  * @Date: 2022-08-17 17:18:15  * @LastEditors: paoluo  * @LastEditTime: 2022-08-17 17:49:54  */ 添加部分注释

--- code/http/httpconn.cpp
+++ code/http/httpconn.cpp
@@ -1,8 +1,10 @@
 /*
- * @Author       : mark
- * @Date         : 2020-06-15
- * @copyleft Apache 2.0
- */ 
+ * @Author: paoluo
+ * @Date: 2022-03-09 22:38:23
+ * @LastEditors: paoluo
+ * @LastEditTime: 2022-08-17 17:43:38
+ */
+
 #include "httpconn.h"
 using namespace std;
 
